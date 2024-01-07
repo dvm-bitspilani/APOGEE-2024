cc83e28d7332bc1c7461e510c0d7fb63569aed7d
yoshicon <shreyaskgantayet908@gmail.com>
2024-01-08 00:52:33 +0530

    Update : Insights

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.scss'
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimationWrapper from './AnimationWrapper';
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
