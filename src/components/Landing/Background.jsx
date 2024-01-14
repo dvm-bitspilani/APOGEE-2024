0064775045106ec3e11c575b1697ce70e86b9420
Luv Gupta <lg5122003@gmail.com>
2024-01-14 13:56:24 +0530

    change: texture background

--- src/components/Landing/Background.jsx
+++ src/components/Landing/Background.jsx
@@ -2,7 +2,7 @@ import { Environment, Sphere } from "@react-three/drei";
 import { LayerMaterial } from "lamina";
 import * as THREE from "three";
 import { TextureLoader } from 'three';
-import TextureMap from "../../../public/images/texture-bg.jpg";
+import TextureMap from "../../../public/images/nebula-bg.jpg";
 const Background = () => {
   const textureSphereBg = new TextureLoader().load(TextureMap);
   textureSphereBg.anisotropy = 16;
--- src/components/Landing/Experience.jsx
+++ src/components/Landing/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, Sphere } from "@react-three/drei";
+import { OrbitControls, Sphere, PerspectiveCamera } from "@react-three/drei";
 import Background from "./Background";
 import * as THREE from "three";
 import TextureMap from "../../../public/images/texture-bg.jpg";
@@ -7,6 +7,11 @@ function Experience() {
   return (
     <>
       <OrbitControls />
+      {/* <PerspectiveCamera
+              position={[-10, -10, 5]}
+              fov={50}
+              makeDefault
+            /> */}
       {/* <Sphere scale={[100, 100, 100]}>
         <meshBasicMaterial
           side={THREE.BackSide} // Render the material on the back side of the geometry (inside)
