1815e4f2c32803d4e379731d0c3cb7216c071e8b
yoshicon <shreyaskgantayet908@gmail.com>
2024-01-19 01:50:29 +0530

    Style : Register animation

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, scale : 1 }}
-      exit={{ opacity: 0, scale: 5, transition: { duration: 1, ease: "easeInOut" } }}
+      exit={{ opacity: 0, transition: { duration: 1, ease: "easeInOut" } }}
       transition={{ duration: 2, ease: "easeInOut" }}
     >
       <Canvas camera={{ fov: 75, position: [0, 0, 0] }}>
