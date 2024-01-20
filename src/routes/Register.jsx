bb8f87bb42d81b502b398ec5fd73e291a3d7f71f
Ritikchandra <ritik.chandra009@gmail.com>
2024-01-20 16:26:41 +0530

    update regpage

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -7,8 +7,8 @@ import { useNavigate } from "react-router-dom";
 export default function Register() {
   const navigate = useNavigate();
   const handleHomeClick = () => {
-    console.log("first");
-    navigate("/");
+    console.log('first');
+    navigate('/');
   };
   return (
     <motion.div
@@ -19,8 +19,9 @@ export default function Register() {
       className={styles.pageContainer}
     >
       <span className={styles.heading}>REGISTRATION</span>
-      <img src="/images/Left helm.png" alt="" className={styles.leftHelm} />
-      <img src="/images/Right helm.png" alt="" className={styles.rightHelm} />
+      <img src="/images/Left helm.png" alt="" className={styles.leftHelm}/>
+      <img src="/images/Right helm.png" alt="" className={styles.rightHelm}/>
+      <div className={styles.topContainer}>
       <img src="/images/leftMobileReg.png" alt="" className={styles.leftTop} />
       <img
         src="/images/rightMobileReg.png"
@@ -28,29 +29,27 @@ export default function Register() {
         className={styles.rightTop}
       />
       <img src="/images/topMobile.png" alt="" className={styles.middleTop} />
+      <div className={styles.homeBtn} onClick={handleHomeClick}>
+        <span>HOME</span>
+        </div>
+      </div>
       <div className={styles.pageWrapper}>
         <img src="/images/Top HUD.svg" alt="" className={styles.hud} />
         <div className={styles.content}>
-          <div className={styles.homeBtn}>
-            <span>HOME</span>
-          </div>
-          <div className={styles.imgContainer}>
-            <img
-              src="/images/globeReg.png"
-              alt=""
-              className={styles.globeImage}
-            />
-          </div>
+        <div className={styles.homeBtn}  onClick={handleHomeClick}>
+        <span>HOME</span>
+        </div>
+        <div className={styles.imgContainer}>
+          <img src="/images/globeReg.png" alt="" className={styles.globeImage}/>
+        </div>
           <div className={styles.formContainer}>
             <MyForm />
           </div>
-        </div>
-        <div className={styles.mobileContent}>
-          <button className={styles.homeBtn} onClick={handleHomeClick}>
-            <span>HOME</span>
-          </button>
-          <MyForm2 />
-        </div>
+          
+      </div>
+      <div className={styles.mobileContent}>
+        <MyForm2/>
+      </div>
       </div>
     </motion.div>
   );
