12f281aaeb4fc53d216ebb34587f394a1dfb72e6
yoshicon <shreyaskgantayet908@gmail.com>
2024-01-21 03:23:27 +0530

    Fix : Camera FOV

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -10,28 +10,28 @@ import EffectComposerLayer from "../components/EffectComposer";
 // import Background from "../components/Landing/Background";
 import Experience from "../components/Landing/Experience";
 
-
 function App() {
-
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1}}
+      animate={{ opacity: 1 }}
       exit={{ opacity: 0, transition: { duration: 1, ease: "easeInOut" } }}
       transition={{ duration: 2, ease: "easeInOut" }}
     >
-      <Canvas camera={{ fov: 75, position: [0, 0, 0] }}>
-        <EffectComposerLayer />
-        <Suspense fallback={<Loader />}>
-          {/* <OrbitControls /> */}
-          <ambientLight intensity={0.5}/>
-          {/* <directionalLight position={[10, 10, 5]} intensity={1} /> */}
-          {/* <Scene /> */}
-          {/* <axesHelper args={[5]} /> */}
-          <Experience />
-        </Suspense>
-        <Stats />
-      </Canvas>
+      <Suspense fallback={<span>loading...</span>}>
+        <Canvas camera={{ fov: 50, position: [0, 0, 0] }}>
+          <EffectComposerLayer />
+          <Suspense fallback={<Loader />}>
+            {/* <OrbitControls /> */}
+            <ambientLight intensity={0.5} />
+            {/* <directionalLight position={[10, 10, 5]} intensity={1} /> */}
+            {/* <Scene /> */}
+            {/* <axesHelper args={[5]} /> */}
+            <Experience />
+          </Suspense>
+          <Stats />
+        </Canvas>
+      </Suspense>
       <Hud />
     </motion.div>
   );
