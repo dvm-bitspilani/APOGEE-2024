dbbccdfead7628a6da0e5105d19836a476381b5a
yoshicon <shreyaskgantayet908@gmail.com>
2024-01-22 12:01:49 +0530

    Fix : Background visibility issues

--- src/components/Landing/Background.jsx
+++ src/components/Landing/Background.jsx
@@ -5,13 +5,24 @@ import { TextureLoader } from "three";
 import TextureMap from "/images/nebula.jpg";
 import { Stars } from "@react-three/drei";
 
+import { useControls } from "leva";
+
 const Background = () => {
   const textureSphereBg = new TextureLoader().load(TextureMap);
   textureSphereBg.anisotropy = 16;
 
+  const {rotation} = useControls("Background", {
+    rotation: {
+      value: [1, -1, 0],
+      step: 0.1,
+      min: -Math.PI * 2,
+      max: Math.PI * 2,
+    },
+  });
+
   return (
     <>
-      <Sphere args={[150, 40, 40]} rotateX={180}>
+      <Sphere args={[150, 40, 40]} rotation={rotation}>
         <meshBasicMaterial
           attach="material"
           side={THREE.BackSide}
