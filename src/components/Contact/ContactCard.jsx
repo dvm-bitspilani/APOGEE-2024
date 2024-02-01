39c44e07256024188ebeb33d186c97ffef39b27f
Akshit <f20220429@pilani.bits-pilani.ac.in>
2024-02-02 00:28:53 +0530

    modify: contact page

--- src/components/Contact/ContactCard.jsx
+++ src/components/Contact/ContactCard.jsx
@@ -2,16 +2,27 @@ import * as styles from "@styles/Contact.module.scss";
 import React from "react";
 import phoneIcon from "../../assets/contact/phone.svg";
 import mailIcon from "../../assets/contact/mail.svg";
-// import sampleImage from "../../assets/contact/sample.png"
-// import vedant from "../../assets/contact/vedant.png"
-// import sachika from "../../assets/contact/sachika.png"
-// import dharun from "../../assets/contact/dharun.png"
-// import yash from "../../assets/contact/yash.png"
-// import tushar from "../../assets/contact/tushar.png"
-// import noname from "../../assets/contact/noname.png"
-// import lamba from "../../assets/contact/lamba.png"
+import sample from "../../assets/contact/sample.png"
+import vedant from "../../assets/contact/vedant.png"
+import sachika from "../../assets/contact/sachika.png"
+import dharun from "../../assets/contact/dharun.png"
+import yash from "../../assets/contact/yash.png"
+import tushar from "../../assets/contact/tushar.png"
+import noname from "../../assets/contact/noname.png"
+import lamba from "../../assets/contact/lamba.png"
 
 export default function ContactCard({ image, dept, name, phone, email }) {
+
+    const imageMap = {
+        vedant: vedant,
+        sachika: sachika,
+        dharun: dharun,
+        yash: yash,
+        tushar: tushar,
+        noname: noname,
+        lamba: lamba,
+        sample: sample
+    };
 
     const handlePhoneClick = () => {
         window.location.href = `tel:${phone}`;
@@ -24,7 +35,7 @@ export default function ContactCard({ image, dept, name, phone, email }) {
     return (
         <div className={styles.cardContainer}>
             <div className={styles.imgContainer}>
-                <img src={image} alt="" draggable='false' />
+                <img src={imageMap[image]} alt="" draggable='false' />
             </div>
             <div className={styles.dept}>{dept}</div>
             <div className={styles.name}>{name}</div>
