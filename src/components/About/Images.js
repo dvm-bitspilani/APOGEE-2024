71b16a30a459003e304e871ce27b707148907cde
Luv Gupta <lg5122003@gmail.com>
2024-02-12 22:05:56 +0530

    chore: change videos

--- src/components/About/Images.js
+++ src/components/About/Images.js
@@ -1,24 +1,23 @@
 export const IMAGES = [
-
     {
         id: 0,
         videoSrc:
-            "https://www.youtube.com/embed/Mdhw5tI7HgE?si=Z2WNrhu5q8iyGREw&amp;controls=0",
+            "https://www.youtube.com/embed/TY7h1Wnqb_A?si=RaV3ut6qiT7WHrrT",
         videoTitle:
-            "APOGEE '22 | The Encrypted Dimension"
+            "APOGEE '24 | A Celestial Epiphany"
     },
     {
         id: 1,
         videoSrc:
-            "https://www.youtube.com/embed/3lbRUfKTwWc?si=5QJdzyaTdLfk8zct",
+            "https://www.youtube.com/embed/vEhXhoynQLc?si=azysTYvJ9YcPaFPN",
         videoTitle:
-            "BOSM '18 | After Movie"
+            "APOGEE '23 | Official Aftermovie"
     },
     {
         id: 2,
         videoSrc:
-            "https://www.youtube.com/embed/1e1n0_EhEXU?si=S-GyOAmwsfnPoZ3q",
+            "https://www.youtube.com/embed/Mdhw5tI7HgE?si=Z2WNrhu5q8iyGREw&amp;controls=0",
         videoTitle:
-            "BOSM '17 | After Movie"
+            "APOGEE '22 | The Encrypted Dimension"
     },
 ]
