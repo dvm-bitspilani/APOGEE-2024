25254499c113f46bb1fb2edd8730cf4ec7457bf9
Luv Gupta <117029895+LuvGuptaa@users.noreply.github.com>
2024-02-27 15:05:38 +0530

    fix: register URL

--- src/components/Form/MyForm.jsx
+++ src/components/Form/MyForm.jsx
@@ -111,7 +111,7 @@ const MyForm = () => {
       console.log('Form Values:',submitValues);
   
       const response = await axios.post(
-        import.meta.env.VITE_TEST_REGISTER_URL,{
+        import.meta.env.VITE_REGISTER_URL,{
           ...submitValues,
         });
         if (response) {
