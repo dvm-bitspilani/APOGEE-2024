6529820842336a18deb81dec3fd54ac2254b546a
Ritikchandra <114809062+Ritikchandra@users.noreply.github.com>
2024-03-25 18:49:51 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,10 +2,21 @@ import { Html, useProgress } from "@react-three/drei";
 
 export default function Loader() {
   const { progress } = useProgress();
+  const displayProgess = Math.ceil(progress)
   // console.log(progress);
   return (
-    <Html center fullscreen>
-      {progress} % loaded
+    <Html fullscreen>
+      <div style={{
+        // width:"100%",
+        // height:"100%",
+        position:"absolute",
+        top:"50%",
+        left:"50%",
+        transform:"translateY(-50%) translateX(-50%)",
+        fontSize:"1.5rem"
+      }}>
+      {displayProgess} % Loaded
+      </div>
     </Html>
   );
 }
