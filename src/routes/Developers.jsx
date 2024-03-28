e2f9aa44460571095320c3feac462f37a44b9225
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-03-28 16:04:51 +0530

    Made the HUD
    - Added images for HUD
    - Made HUD responsive
    - Created SCSS file

--- src/routes/Developers.jsx
+++ src/routes/Developers.jsx
@@ -1,7 +1,57 @@
+import { motion } from "framer-motion";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as styles from "../styles/Developers.module.scss";
+import devTitle from "/images/DevelopersTitleText.png";
+
 export default function Developers() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(`/`);
+    };
     return (
-        <>
-            dev page
-        </>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, transition: { duration: 1, ease: "easeInOut" } }}
+            transition={{ duration: 2, ease: "easeInOut" }}
+            className={styles.pageContainer}
+        >
+            <img
+                draggable={false}
+                className={styles.lefthelm}
+                src="/images/Left helm.png"
+                alt="left helm"
+                style={{ position: "fixed" }}
+            />
+            <img
+                draggable={false}
+                className={styles.righthelm}
+                src="/images/Right helm.png"
+                alt="right helm"
+                style={{ position: "fixed" }}
+            />
+            <img
+                draggable={false}
+                className={styles.tophud}
+                src="/images/devPageTopHUD.png"
+                alt="top hud"
+            />
+            <button
+                id="ham-menu-button"
+                className={styles.hamMenuButton}
+                onClick={handleClick}
+            >
+                <span>HOME</span>
+            </button>
+            <img
+                draggable={false}
+                className={styles.bottomHUD}
+                src="/images/devPageBottomHUD.png"
+                alt="top hud"
+            />
+        </motion.div>
     )
 }
