ed02bdd7ab18cfad16501411ce899a3c556f3ce2
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-03-30 06:05:05 +0530

    Fixed console error

--- src/routes/Developers.jsx
+++ src/routes/Developers.jsx
@@ -115,7 +115,7 @@ export default function Developers() {
                                 alt="design"
                             />
                             <svg className={styles.folder} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 402 274" fill="none">
-                                <path d="M400 45.0376L399.601 236.677L366.9 272L16.3567 271.188L2 255.353V16.6165L17.9519 2H237.291L263.611 29.609H384.048L400 45.0376Z" stroke="#4DE5FD" stroke-width="3.36963" />
+                                <path d="M400 45.0376L399.601 236.677L366.9 272L16.3567 271.188L2 255.353V16.6165L17.9519 2H237.291L263.611 29.609H384.048L400 45.0376Z" stroke="#4DE5FD" strokeWidth="3.36963" />
                             </svg>
                         </div>
                         <div>
@@ -126,7 +126,7 @@ export default function Developers() {
                                 alt="backend"
                             />
                             <svg className={styles.folder} viewBox="0 0 402 274" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M400 228.962L399.601 37.3233L366.9 2L16.3567 2.81204L2 18.6466V257.383L17.9519 272H237.291L263.611 244.391H384.048L400 228.962Z" stroke="#4DE5FD" stroke-width="3.36963" />
+                                <path d="M400 228.962L399.601 37.3233L366.9 2L16.3567 2.81204L2 18.6466V257.383L17.9519 272H237.291L263.611 244.391H384.048L400 228.962Z" stroke="#4DE5FD" strokeWidth="3.36963" />
                             </svg>
                         </div>
                     </div>
@@ -134,10 +134,10 @@ export default function Developers() {
                         className={styles.secondCol}
                     >
                         <svg className={styles.topLeft} viewBox="0 0 136 55" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M134 54L109.5 2H0" stroke="#4DE5FD" stroke-width="3" />
+                            <path d="M134 54L109.5 2H0" stroke="#4DE5FD" strokeWidth="3" />
                         </svg>
                         <svg className={styles.topRight} viewBox="0 0 136 55" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2 54L26.5 2H136" stroke="#4DE5FD" stroke-width="3" />
+                            <path d="M2 54L26.5 2H136" stroke="#4DE5FD" strokeWidth="3" />
                         </svg>
 
                         <svg className={styles.dvmLogo} viewBox="0 0 119 151" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -145,12 +145,6 @@ export default function Developers() {
                             <path d="M118.059 -0.000274658L103.255 10.7326V91.599L48.666 133.605L0.368164 99.0009V116.211L48.666 151L118.059 96.9654V-0.000274658Z" />
                         </svg>
 
-                        {/* <img
-                        draggable={false}
-                        className={styles.arcReactor}
-                        src="/images/arcReactorNoLogo.png"
-                        alt="arc reactor"
-                    /> */}
                         <div
                             className={styles.arcReacter}
                         >
@@ -175,13 +169,13 @@ export default function Developers() {
                         </div>
 
                         <svg className={styles.botLeft} viewBox="0 0 136 55" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M134 1.5L109.5 53.5H0" stroke="#4DE5FD" stroke-width="3" />
+                            <path d="M134 1.5L109.5 53.5H0" stroke="#4DE5FD" strokeWidth="3" />
                         </svg>
                         <svg className={styles.botRight} viewBox="0 0 136 55" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1.5 1.5L26 53.5H135.5" stroke="#4DE5FD" stroke-width="3" />
+                            <path d="M1.5 1.5L26 53.5H135.5" stroke="#4DE5FD" strokeWidth="3" />
                         </svg>
-
                     </div>
+
                     <div
                         className={styles.thirdCol}
                     >
@@ -193,7 +187,7 @@ export default function Developers() {
                                 alt="frontend"
                             />
                             <svg className={styles.folder} viewBox="0 0 402 274" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 45.0376L2.3988 236.677L35.1002 272L385.643 271.188L400 255.353V16.6165L384.048 2H164.709L138.389 29.609H17.9519L2 45.0376Z" stroke="#4DE5FD" stroke-width="3.36963" />
+                                <path d="M2 45.0376L2.3988 236.677L35.1002 272L385.643 271.188L400 255.353V16.6165L384.048 2H164.709L138.389 29.609H17.9519L2 45.0376Z" stroke="#4DE5FD" strokeWidth="3.36963" />
                             </svg>
                         </div>
                         <div>
@@ -204,7 +198,7 @@ export default function Developers() {
                                 alt="video"
                             />
                             <svg className={styles.folder} viewBox="0 0 402 274" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 228.962L2.3988 37.3233L35.1002 2L385.643 2.81204L400 18.6466V257.383L384.048 272H164.709L138.389 244.391H17.9519L2 228.962Z" stroke="#4DE5FD" stroke-width="3.36963" />
+                                <path d="M2 228.962L2.3988 37.3233L35.1002 2L385.643 2.81204L400 18.6466V257.383L384.048 272H164.709L138.389 244.391H17.9519L2 228.962Z" stroke="#4DE5FD" strokeWidth="3.36963" />
                             </svg>
                         </div>
                     </div>
@@ -241,6 +235,7 @@ export default function Developers() {
                     />
                     <img
                         draggable={false}
+                        style={{ marginBottom: '10px' }}
                         src="/images/videoMobile.png"
                         alt="video"
                     />
